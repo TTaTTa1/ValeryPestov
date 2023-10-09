9d765c1a9a6447f9922d5510350e8564cfb6680d
PestovVA <141711731+TTaTTa1@users.noreply.github.com>
2023-10-09 08:45:45 +0500

    add semi_5 выполнение второго задания

--- HomeVorks/semmi_5/Program.cs
+++ HomeVorks/semmi_5/Program.cs
@@ -42,3 +42,39 @@ int size1 = Convert.ToInt32(Console.ReadLine());
 int[] myArray1 = CreateRandomArray1(size1);
 PrintArray1(myArray1);
 Console.WriteLine(CounterEvenNumbers(myArray1));
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+Console.WriteLine("Task 36: Set a one-dimensional array filled with random numbers.\n" 
++"Find the sum of the elements standing in odd positions.");
+
+int[] CreateRandomArray2(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+int SumEvenPozition(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i += 2) sum += array[i];
+    return sum;
+}
+
+Console.WriteLine("Input array size: ");
+int size2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input array minimal value: ");
+int minValue2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input array maximal value: ");
+int maxValue2 = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray2 = CreateRandomArray2(size2, minValue2, maxValue2);
+PrintArray1(myArray2);
+Console.WriteLine(SumEvenPozition(myArray2));
+
