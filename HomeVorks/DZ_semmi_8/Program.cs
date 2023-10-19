f7d81032c2b0acee6578359a62830d7c86d24866
PestovVA <141711731+TTaTTa1@users.noreply.github.com>
2023-10-19 09:08:05 +0500

    решение задачи 68

--- HomeVorks/DZ_semmi_8/Program.cs
+++ HomeVorks/DZ_semmi_8/Program.cs
@@ -45,3 +45,24 @@ int numN = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input M: ");
 int numM = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"M = {numM}; N = {numN} -> {SumNumbers(numN, numM)}");
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n)
+
+Console.WriteLine("Task 68. Write a program for calculating the Ackerman function using recursion.");
+
+int Ackermann(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return Ackermann(m - 1, 1);
+else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+Console.WriteLine("Введите значения m и n, где m и n > 0:");
+int Ack1 = Convert.ToInt32(Console.ReadLine());
+int Ack2 = Convert.ToInt32(Console.ReadLine());
+
+int result = Ackermann(Ack1, Ack2);
+Console.WriteLine($"Ackermann({Ack1}, {Ack2}) = {result}");
