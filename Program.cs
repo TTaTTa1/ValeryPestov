5cbf8e03e8669147b706593d5e3db317ac820c96
PestovVA <141711731+TTaTTa1@users.noreply.github.com>
2024-02-25 15:00:53 +0500

    Добавлены функции CreateArray и ShowArray для ввода и отображения массива

--- Program.cs
+++ Program.cs
@@ -20,3 +20,22 @@ void ShowArray(string[] array)
         Console.Write(array[i] + " ");
     Console.WriteLine();
 }
+string[] ResultArray(string[] array)
+{
+    int c = 0;
+    int j = 0;
+    for (int i = 0; i < array.Length; i += 1)
+    {
+        if (array[i].Length <= 3) c += 1;
+    }
+    string[] arrayRes = new string[c];
+    for (int i = 0; i < array.Length; i += 1)
+     {
+        if (array[i].Length <= 3)
+        {
+            arrayRes[j] = array[i];
+            j += 1;
+        }
+    }
+    return arrayRes;
+}
