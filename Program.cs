4ffcb90a6b74190fcd5dec26b9c3fdf3b93435db
PestovVA <141711731+TTaTTa1@users.noreply.github.com>
2024-02-25 14:58:50 +0500

    Добавлены функции CreateArray и ShowArray для ввода и отображения массива

--- Program.cs
+++ Program.cs
@@ -2,3 +2,21 @@
 // длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
+string[] CreateArray()
+{
+    Console.Write("Введите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+    for (int i = 0; i < size; i += 1)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i += 1)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
